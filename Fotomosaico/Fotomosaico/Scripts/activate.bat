3d76e311f3a7d46ef2b50a3b7dacb321e0a82cbb
robdrr <robotodrr@gmail.com>
2025-02-14 01:57:41 -0600

    Primer commit

--- Fotomosaico/Fotomosaico/Scripts/activate.bat
+++ Fotomosaico/Fotomosaico/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Fotomosaico\fotomosaico"
+set "VIRTUAL_ENV=C:\Fotomosaico\Fotomosaico\Fotomosaico\Fotomosaico"
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(fotomosaico) %PROMPT%
+set PROMPT=(Fotomosaico) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
-set "VIRTUAL_ENV_PROMPT=(fotomosaico) "
+set "VIRTUAL_ENV_PROMPT=(Fotomosaico) "
 
 :END
 if defined _OLD_CODEPAGE (
