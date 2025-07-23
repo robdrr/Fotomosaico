16823a54b7a538d9c973bd6bc9111f633d964780
Roberto Carlos Rodríguez <146799060+robdrr@users.noreply.github.com>
2025-07-22 19:56:54 -0600

    Update Euclidiana.py

--- Fotomosaico/Euclidiana.py
+++ Fotomosaico/Euclidiana.py
@@ -13,7 +13,7 @@ def buscar_color_mas_cercano(r_target, g_target, b_target, tabla):
     """
     
     #     query = """
-    #     SELECT Name, R, G, B, 
+    #     SELECT Name, R, G, B,       
     #            SQRT(POW(R - %s, 2) + POW(G - %s, 2) + POW(B - %s, 2)) AS distancia
     #     FROM promedio_rgb
     #     ORDER BY distancia ASC
