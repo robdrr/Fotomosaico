1ea6bda6ece68ae7e383057fc861a6602aa2b11e
Roberto Carlos Rodríguez <146799060+robdrr@users.noreply.github.com>
2025-07-22 19:56:32 -0600

    Update Euclidiana.py

--- Fotomosaico/Euclidiana.py
+++ Fotomosaico/Euclidiana.py
@@ -5,7 +5,7 @@ import random
 def buscar_color_mas_cercano(r_target, g_target, b_target, tabla):
       # Asegúrate de validar el nombre para evitar SQL injection
     query = f"""
-        SELECT Name, R, G, B, 
+        SELECT Name, R, G, B,   
             SQRT(POW(R - %s, 2) + POW(G - %s, 2) + POW(B - %s, 2)) AS distancia
         FROM {tabla}  
         ORDER BY distancia ASC
